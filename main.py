21f8cff5d1a4874b901339e5dc190eccb868f0a7
papidox <papipaidetodos@icloud.com>
2025-06-14 09:44:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 from bot.handlers.start import start
 
-app = ApplicationBuilder().token("YOUR_BOT_TOKEN_HERE").build()
+app = ApplicationBuilder().token("7565675198:AAFTGLzXZa88KRxBmjob4fMDuP2tUrt6Dvg").build()
 app.add_handler(CommandHandler("start", start))
 
 print("Bot rodando...")
