0d2a08b62aeea3e5b3ef5b95f4d24259e65a3112
papidox <207430236+papidox@users.noreply.github.com>
2025-06-14 17:53:54 -0300

    kk

--- bot/handlers/suporte.py
+++ bot/handlers/suporte.py
@@ -4,14 +4,9 @@ from telegram.ext import ContextTypes
 
 async def suporte(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.callback_query.message.reply_text(
-        "🛠️ *Suporte Técnico*
-
-"
-        "Caso tenha alguma dúvida, envie sua pergunta diretamente ao nosso suporte humano.
-
-"
-        "📩 Telegram: @pkmetodos_suporte
-"
+        "🛠️ *Suporte Técnico*\n\n"
+        "Caso tenha alguma dúvida, envie sua pergunta diretamente ao nosso suporte humano.\n\n"
+        "📩 Telegram: @pkmetodos_suporte\n"
         "⏱️ Atendimento: 09h às 22h (todos os dias)",
         parse_mode='Markdown'
     )
